77ce748a822e294f7a2767fcae4aabb48f847841
YuCheng-Chang <gsn984309.eed06@nctu.edu.tw>
2021-06-16 15:11:45 +0000

    add fasttext embedding

--- embedding.py
+++ embedding.py
@@ -9,13 +9,15 @@ def get_embedding(query):
         ''' query word already in bible '''
         return embeddings[query]
     else:
+        print('1-------------')
         model = KeyedVectors.load_word2vec_format('crawl-300d-2M.vec', binary=False)
         word_vectors = model.wv
         # -- this saves space, if you plan to use only, but not to train, the model:
         del model
 
         # -- do your work:
-        word_vectors.most_similar(query, topn=10) 
+        similar_words = word_vectors.most_similar(query, topn=10) 
+        print(similar_words)
 if __name__ == '__main__':
     get_embedding('technology')
         
