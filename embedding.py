2c02b2e46336b53a6c10b697f641a98ea4295ddb
LiyangTseng <k00441tsengliyung@gmail.com>
2021-06-17 06:19:58 +0000

    synonym works for query

--- embedding.py
+++ embedding.py
@@ -1,29 +1,35 @@
 import os
 from gensim.models import KeyedVectors
 from flair.embeddings import WordEmbeddings
-from gensim.models.keyedvectors import _try_upgrade
 from tqdm import tqdm
 import sys
 
 def get_embedding(query):
     query = query.lower()
     embedding_path = os.path.join('w2v', "bible_word2vec_org")
-    print('Loading pretrained bible embeddings from {} ...'.format(embedding_path))
+    print('Checking if query in bible ...')
     embeddings = KeyedVectors.load_word2vec_format(embedding_path)
     if query in embeddings.vocab.keys():
         ''' query word already in bible '''
+        print('Query exists in Bible')
         return embeddings[query]
     else:
         ''' query word not in bible '''
+        print('Query not found in Bible')
         fasttext_embeddings = WordEmbeddings('news')
         synonyms = fasttext_embeddings.precomputed_word_embeddings.most_similar(
-            query, topn=500000)
-        for synonym in tqdm(synonyms):
-            if synonym in embeddings.vocab.keys():
-                print('Using "{}" as synonym'.format(synonym))
-                return embeddings[synonym]   
+            query, topn=50000)
+        synonym_found = False
+        for synonym_tuple in tqdm(synonyms, desc='Searching Synonyms'):
+            if synonym_tuple[0] in embeddings.vocab.keys():
+                synonym_found = True
+                break
         
-        raise Exception('No synonym in voab')
+        if synonym_found:
+            print('Using "{}" as synonym'.format(synonym_tuple[0]))
+            return embeddings[synonym_tuple[0]]   
+        else:
+            raise Exception('No synonyms found in Bible')
 
 if __name__ == '__main__':
     get_embedding(sys.argv[1])
