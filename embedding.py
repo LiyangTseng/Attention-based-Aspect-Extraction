e331ae9937820d43aa264e4a5742ee31be373c6d
LiyangTseng <k00441tsengliyung@gmail.com>
2021-06-17 03:34:03 +0000

    add synonym mechanism

--- embedding.py
+++ embedding.py
@@ -1,23 +1,30 @@
 import os
 from gensim.models import KeyedVectors
+from flair.embeddings import WordEmbeddings
+from gensim.models.keyedvectors import _try_upgrade
+from tqdm import tqdm
+import sys
 
 def get_embedding(query):
+    query = query.lower()
     embedding_path = os.path.join('w2v', "bible_word2vec_org")
-    print('loading word bible embeddings from {} '.format(embedding_path))
+    print('Loading pretrained bible embeddings from {} ...'.format(embedding_path))
     embeddings = KeyedVectors.load_word2vec_format(embedding_path)
     if query in embeddings.vocab.keys():
         ''' query word already in bible '''
         return embeddings[query]
     else:
-        print('1-------------')
-        model = KeyedVectors.load_word2vec_format('crawl-300d-2M.vec', binary=False)
-        word_vectors = model.wv
-        # -- this saves space, if you plan to use only, but not to train, the model:
-        del model
-
-        # -- do your work:
-        similar_words = word_vectors.most_similar(query, topn=10) 
-        print(similar_words)
-if __name__ == '__main__':
-    get_embedding('technology')
+        ''' query word not in bible '''
+        fasttext_embeddings = WordEmbeddings('news')
+        synonyms = fasttext_embeddings.precomputed_word_embeddings.most_similar(
+            query, topn=500000)
+        for synonym in tqdm(synonyms):
+            if synonym in embeddings.vocab.keys():
+                print('Using "{}" as synonym'.format(synonym))
+                return embeddings[synonym]   
         
+        raise Exception('No synonym in voab')
+
+if __name__ == '__main__':
+    get_embedding(sys.argv[1])
+    # get_embedding(input())    
--- train_wv.py
+++ train_wv.py
@@ -19,18 +19,13 @@ def get_wv(bible_kjv_sents):
     vocab_path = os.path.join(w2v_folder, "bible_word2vec_vocalbulary")
     
     if not os.path.exists(embedding_path):
-        print('generating word embeddings')
-        # bible_kjv_sents = gutenberg.sents('bible-kjv.txt')
-        # #  
-        # discard_punctuation_and_lowercased_sents = [[word.lower() for word in sent if word not in punctuation] for sent in bible_kjv_sents]
-        
-        # bible_kjv_word2vec_model = word2vec.Word2Vec(discard_punctuation_and_lowercased_sents, min_count=1, size=200)
+        print('Generating word embeddings')
         bible_kjv_word2vec_model = word2vec.Word2Vec(bible_kjv_sents, min_count=1, size=200)
         bible_kjv_word2vec_model.wv.save_word2vec_format(embedding_path, vocab_path)
-        print('bible word embeddings generated and stored at {}'.format(embedding_path))
+        print('Bible word embeddings generated and stored at {}'.format(embedding_path))
         return  bible_kjv_word2vec_model.wv
     else:
-        print('load word bible embeddings from {} '.format(embedding_path))
+        print('Loading pretrained bible embeddings from {} ...'.format(embedding_path))
         return KeyedVectors.load_word2vec_format(embedding_path)
 
 if __name__ == '__main__':
